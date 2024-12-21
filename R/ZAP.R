a07b4e2e6561406d5c4825bb717ca4b4203f67d2
Mikis Stasinopoulos <dmh.stasinopoulos@gmail.com>
2024-12-21 13:48:18 +0000

    ZAP

--- R/ZAP.R
+++ R/ZAP.R
@@ -69,7 +69,9 @@ dZAP<-function(x, mu = 5, sigma = 0.1, log = FALSE)
           sigma <- rep(sigma, length = ly)
              mu <- rep(mu, length = ly)
           logfy <- rep(0, ly)
-          logfy <- ifelse((x==0), log(sigma), log(1-sigma) + dPO(x,mu,log=T) - log(1-dPO(0,mu)) )          
+          logfy <-  log(1-sigma) + dPO(x,mu,log=T) - log(1-dPO(0,mu))
+    logfy[x==0] <- log(sigma)  
+ #         logfy <- ifelse((x==0), log(sigma), log(1-sigma) + dPO(x,mu,log=T) - log(1-dPO(0,mu)) )          
           if(log == FALSE) fy <- exp(logfy) else fy <- logfy
           fy[x < 0] <- 0
           fy[x == Inf] <- 0
@@ -91,8 +93,9 @@ pZAP <- function(q, mu = 5, sigma = 0.1, lower.tail = TRUE, log.p = FALSE)
           cdf <- rep(0,ly)
          cdf1 <- ppois(q, lambda = mu, lower.tail = TRUE, log.p = FALSE)
          cdf2 <- ppois(0, lambda = mu, lower.tail = TRUE, log.p = FALSE)
-         cdf3 <- sigma+((1-sigma)*(cdf1-cdf2)/(1-cdf2))
-          cdf <- ifelse((q==0),sigma,  cdf3)
+         cdf <- sigma+((1-sigma)*(cdf1-cdf2)/(1-cdf2))
+  cdf[q==0]  <- sigma
+  #        cdf <- ifelse((q==0),sigma,  cdf3)
          if(lower.tail == TRUE) cdf <- cdf else cdf <-1-cdf
          if(log.p==FALSE) cdf <- cdf else cdf <- log(cdf) 
   cdf[q < 0] <- 0 
@@ -105,22 +108,25 @@ cdf[q > Inf] <- 1
 ################################################################################
 qZAP <- function(p, mu = 5, sigma = 0.1, lower.tail = TRUE, log.p = FALSE)
   {      
-         if (any(mu <= 0) )  stop(paste("mu must be greater than 0", "\n", ""))           
-         if (any(sigma <= 0) )  stop(paste("sigma must be greater than 0", "\n", "")) 
-         if (log.p == TRUE) p <- exp(p)   else p <- p
-         if (lower.tail == TRUE)  p <- p  else p <- 1 - p
-             ly <- max(length(p),length(mu),length(sigma)) 
-              p <- rep(p, length = ly)      
-          sigma <- rep(sigma, length = ly)
-             mu <- rep(mu, length = ly)
-          pnew  <- (p-sigma)/(1-sigma)
-          pnew2 <- ppois(0, lambda = mu, lower.tail = TRUE, log.p = FALSE)*(1-pnew) + pnew 
-          suppressWarnings(q <- ifelse((pnew > 0 ), qpois(pnew2, lambda = mu, ), 0))
-          q[p == 0] <- 0
-          q[p == 1] <- Inf
-          q[p <  0] <- NaN
-          q[p >  1] <- NaN
-          return(q)  
+if (any(mu <= 0) )  stop(paste("mu must be greater than 0", "\n", ""))           
+if (any(sigma <= 0) )  stop(paste("sigma must be greater than 0", "\n", "")) 
+
+if (log.p == TRUE) p <- exp(p)   else p <- p
+if (lower.tail == TRUE)  p <- p  else p <- 1 - p
+      ly <- max(length(p),length(mu),length(sigma)) 
+       p <- rep(p, length = ly)      
+   sigma <- rep(sigma, length = ly)
+      mu <- rep(mu, length = ly)
+   pnew  <- (p-sigma)/(1-sigma)
+   pnew2 <- ppois(0, lambda = mu, lower.tail = TRUE, log.p = FALSE)*(1-pnew) + pnew 
+#       q <- 0
+#       q[pnew <= 0] <- qpois(pnew2[q[pnew < 0]], lambda = mu)
+  suppressWarnings(q <- ifelse((pnew > 0 ), qpois(pnew2, lambda = mu, ), 0))
+  q[p == 0] <- 0
+  q[p == 1] <- Inf
+  q[p <  0] <- NaN
+  q[p >  1] <- NaN
+  return(q)  
    }
 ################################################################################
 ################################################################################
