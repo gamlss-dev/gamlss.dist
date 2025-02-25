6e46f56491bf83a867b1a7fba4cd207403803b9b
Reto Stauffer <reto.stauffer@uibk.ac.at>
2025-02-25 11:26:19 +0100

    feat: Adding PO config

---
+++ inst/tinytest/config/config-PO.R
@@ -0,0 +1,26 @@
+# -------------------------------------------------------------------
+# Configuration for testing the family object.
+# Used by the tinytest scripts for automated testing.
+# -------------------------------------------------------------------
+
+# Used to add some delta to test for 'out of support' values
+eps <- sqrt(.Machine$double.eps)
+
+# Setting up configuration
+res <- list(
+    type    = "Discrete",
+    support = c(-Inf, Inf),
+
+    # Name of the parameters
+    params = c("mu", "sigma"),
+
+    # Valid and invalid response values
+    y     = list(valid = c(0, 1, 30), invalid = c(-1, -eps)),
+
+    # TODO(R): PO currently does not allow for '0' (base R does)
+    mu    = list("identity" = list(range = c(eps, Inf), valid = c(eps, 1, 30), invalid = c(-eps, 0)),
+                 "inverse"  = list(range = c(eps, Inf), valid = c(eps, 1, 30), invalid = c(-eps, 0)),
+                 "log"      = list(range = c(eps, Inf), valid = c(eps, 1, 30), invalid = c(-eps, 0)),
+                 "sqrt"     = list(range = c(eps, Inf), valid = c(eps, 1, 30), invalid = c(-eps, 0)))
+)
+
